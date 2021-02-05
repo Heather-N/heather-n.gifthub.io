28e31da3321d9c5bb596f40305c63b99ef2c6d15
Heather-N <nel19042@byui.edu>
2021-02-05 11:21:46 -0700

    Commit

--- Lesson-5/js/script.js
+++ Lesson-5/js/script.js
@@ -2,3 +2,8 @@ function toggleMenu() {
     
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+function currentDate() {
+    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+}
