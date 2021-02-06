72686d7086e440d05e0530a7302f1661fb8c3f81
Heather-N <nel19042@byui.edu>
2021-02-06 11:55:31 -0700

    Commit

--- Lesson-5/js/script.js
+++ Lesson-5/js/script.js
@@ -7,3 +7,15 @@ function currentDate() {
     const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
     document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 }
+
+function topBanner() {
+    let day = new Date();
+    let dayOfWeek = d.getDay();
+
+    if (dayofWeek == 5) {
+        document.getElementById('pancakes').style.display = "block";
+    }
+    else {
+        document.getElementById('pancakes').style.display = "none";
+    }
+}
