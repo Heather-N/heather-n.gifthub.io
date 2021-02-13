b23b5d190267319fb4aa6dec2be0039970304cb5
Heather-N <nel19042@byui.edu>
2021-02-13 15:45:08 -0700

    Commit

--- Lesson-6/js/windchill.js
+++ Lesson-6/js/windchill.js
@@ -1,6 +1,6 @@
 function windChill(){
     let t = parseInt(document.getElementById('temp'));
     let s = parseInt(document.getElementById('windspeed'));
-    let c = Math.round(35.74 + (0.6215 * t) - (35.75 * (math.pow(s, 0.16))) + ((0.4275 * t) * (math.pow(s, 0.16))));
+    let c = Math.round(35.74 + (0.6215 * t) - (35.75 * (Math.pow(s, 0.16))) + ((0.4275 * t) * (Math.pow(s, 0.16))));
     document.getElementById('windchill').innerHTML = c;
 }
