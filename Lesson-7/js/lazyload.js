051c52626b4434222b823a6d98e40f1427d28ef4
Heather-N <nel19042@byui.edu>
2021-02-17 21:06:23 -0700

    Commit

--- Lesson-7/js/lazyload.js
+++ Lesson-7/js/lazyload.js
@@ -1,1 +1,32 @@
 //using code from MDN web docs
+
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
+
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+}
+else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
