c9e8e7eea69d43403cc6113e707e981a5feca33a
Heather-N <nel19042@byui.edu>
2021-02-18 19:49:16 -0700

    Commit

--- Lesson-7/js/lazy.js
+++ Lesson-7/js/lazy.js
@@ -1,25 +1,31 @@
-//used from MDN Web Docs examples
-
 const imagesToLoad = document.querySelectorAll('img[data-src]');
+
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
     image.removeAttribute('data-src');
   };
 };
+
+const imgOptions = {
+  threshold:1,
+  rootMargin:'0px 0px 50px 0px'
+};
+
 if ('IntersectionObserver' in window) {
-  const observer = new IntersectionObserver((items) => {
+  const imgObserver = new IntersectionObserver ((items) => {
     items.forEach((item) => {
       if (item.isIntersecting) {
         loadImages(item.target);
-        observer.unobserve(item.target);
+        imgObserver.unobserve(item.target);
       }
     });
-  });
+  }, imgOptions);
+
   imagesToLoad.forEach((img) => {
-    observer.observe(img);
+    loadImages(img);
   });
-} 
+}
 else {
   imagesToLoad.forEach((img) => {
     loadImages(img);
