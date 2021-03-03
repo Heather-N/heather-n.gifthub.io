19cdf30d25d70ecef39fdc733055b1052e8ee1d5
Heather-N <nel19042@byui.edu>
2021-03-03 11:29:14 -0700

    Commit

---
+++ Lesson-9/js/script.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+    
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+function currentDate() {
+    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+}
+
+function topBanner() {
+    var d = new Date();
+    var n = d.getDay();
+
+    if (n == 5) {
+        document.getElementById('pancakes').style.display = "block";
+    }
+    else {
+        document.getElementById('pancakes').style.display = 'none';
+    }
+}
+
+
+//Storm Severity Label
+function adjustRating(severity) {
+    document.getElementById("rating").innerHTML = severity;
+}
