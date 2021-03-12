680b12923d846d9f78671c17e269b60e0ac68375
Heather-N <nel19042@byui.edu>
2021-03-11 17:13:19 -0700

    Commit

--- Lesson-10/js/weatherapi.js
+++ Lesson-10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=d4bbab983f15105c9250449493f70b92"
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=d4bbab983f15105c9250449493f70b92";
 
 fetch(apiURL)
   .then((response) => 
