2cc530cc55b7808592bd77989224f8967e93e717
Heather-N <nel19042@byui.edu>
2021-03-15 16:01:58 -0600

    Commit

--- Lesson-10/js/prestonforecast.js
+++ Lesson-10/js/prestonforecast.js
@@ -5,12 +5,36 @@ fetch(url)
     .then ((response) =>
         response.json())
     .then((jsObject) => {
-        console.log(jsObject);
-        let day = 0;
-        const dayofWeek = [Sun, Mon, Tue, Wed, Thur, Fri, Sat];
-        const sixList = jsObject.list.filter(time => time.dt_txt.includes('18:00:00'));
+        //console.log(jsObject);
+        let dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-        let d = new Date(jsObject.list[].dt_txt);
-        document.getElementById('dayofweek${day+1}').textContent = dayofweek[d.getDay()];
-        document.getElementById('forecast${day+1}').textContent = jsObject.list[].main.temp;
+        const sixList = jsObject.list.filter(time => time.dt_txt.includes('18:00:00'));
+        let d = new Date(sixList[0].dt_txt);
+        document.getElementById('dayofweek1').textContent = dayofWeek[d.getDay()];
+        document.getElementById('dayofweek2').textContent = dayofWeek[d.getDay() + 1];
+        document.getElementById('dayofweek3').textContent = dayofWeek[d.getDay() + 2];
+        document.getElementById('dayofweek4').textContent = dayofWeek[d.getDay() + 3];
+        document.getElementById('dayofweek5').textContent = dayofWeek[d.getDay() + 4];
+
+        console.log(sixList);
+        sixList.forEach(x => {
+            i = 0;
+            
+            document.getElementById(`forecast${i + 1}`).textContent = Math.round(x.main.temp);
+
+            i++;
+        })
+        for (i=0; i < 5; i++) {
+            if (jsObject.list[i].dt_txt.includes('18:00:00')) {
+                day++;
+                
+                let date = d.getDay();
+                
+            }
+        }
+        
+
+        //let d = new Date(jsObject.list[].dt_txt);
+        //document.getElementById('dayofweek${day+1}').textContent = dayofweek[d.getDay()];
+        //document.getElementById('forecast${day+1}').textContent = jsObject.list[].main.temp;
     })
