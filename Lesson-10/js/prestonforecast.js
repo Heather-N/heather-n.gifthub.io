e03d5bcb0d105a706accffca44bba0def9ff7452
Heather-N <nel19042@byui.edu>
2021-03-16 16:29:10 -0600

    Commit

--- Lesson-10/js/prestonforecast.js
+++ Lesson-10/js/prestonforecast.js
@@ -11,12 +11,11 @@ fetch(url)
         const sixList = jsObject.list.filter(time => time.dt_txt.includes('18:00:00'));
         
         let i = 0;
-        let y = 0;
         console.log(sixList);
         //Using '${i +1}' as shown by Brother Blazzard
         sixList.forEach(x => {
             
-            let d = new Date(sixList[y].dt_txt);
+            let d = new Date(sixList[i].dt_txt);
             document.getElementById(`dayofweek${i + 1}`).textContent = dayofWeek[d.getDay()];
         
             let image = 'https://openweathermap.org/img/w/' + x.weather[0].icon + '.png';
@@ -27,7 +26,6 @@ fetch(url)
             document.getElementById(`forecast${i + 1}`).textContent = Math.round(x.main.temp);
 
             i++;
-            y++;
             
         })
         
